ef2d72f6fa5ed9789a7f5ad058940ee8dd3072b8
wangyang <yangw@opera.com>
2020-11-07 14:32:31 +0800

    learn ch05

---
+++ ch05/instructions/stores/lstore.go
@@ -0,0 +1,21 @@
+package stores
+
+import "jvmgo/ch05/instructions/base"
+import "jvmgo/ch05/rtda"
+
+//Store long into local variable
+type LSTORE struct {
+	base.Index8Instruction
+}
+type LSTORE_0 struct {
+	base.NoOperandsInstruction
+}
+type LSTORE_1 struct {
+	base.NoOperandsInstruction
+}
+type LSTORE_2 struct {
+	base.NoOperandsInstruction
+}
+type LSTORE_3 struct {
+	base.NoOperandsInstruction
+}
